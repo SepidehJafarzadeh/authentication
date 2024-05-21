4b8d1ee92940a0cd9212fab9a4f69cdcf05f7542
SepidehJafarzadeh <sepidehjafarzadeh2004@gmail.com>
2024-05-21 14:00:10 +0330

    urls added

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import LoginView, RegisterView , GoogleAuthView, GoogleAuthCallbackView
+
+app_name = 'accounts'
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('google/', GoogleAuthView.as_view(), name='google'),
+    path('callback/', GoogleAuthCallbackView.as_view(), name='callback'),
+]
